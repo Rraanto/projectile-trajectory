6a66bf664b193359d59b0e319ee15097a1e0979c
Ranto <ranto.rakotondrajoa@gmail.com>
2022-03-06 14:02:17 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# projectile-sim\n Basic simulator of a projectile movement trajectory using tkinter, numpy and matplotlib
---
+++ interface/window.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+"""
+Class Window 
+inherits tkinter.Tk()
+instance of which will serve as the main window of the application
+"""
+
+class Window(tk.Tk):
+    """
+    initializer function 
+    @param height : default to 601, allows to create instance of the class with different height
+    @param width : default to 962, allows to create instance of the class with different width
+    """
+    def __init__(self, title="Window", height=601, width=962):
+        # basic setup
+        super().__init__()
+        self.height = height
+        self.width = width
+        self.title(title)
+        self.geometry(f'{width}x{height}')
+
+        # creating all the necessary widgets (labels, buttons, views, etc...)
+        
+
+# testings 
+if __name__ == "__main__": 
+    x, y = 601, 962
+    my_window = Window(height=x, width=y)
+    my_window.mainloop()
