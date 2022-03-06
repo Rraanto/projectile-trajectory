14f9e90863d30afb29066f5316fb859084575636
syviordf <syviokevinrdf@gmail.com>
2022-03-06 22:11:57 +0300

    first basic interface

--- interface/window.py
+++ interface/window.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import *
 
 """
 Class Window 
@@ -18,14 +21,72 @@ class Window(tk.Tk):
         self.height = height
         self.width = width
         self.title(title)
+
         self.geometry(f'{width}x{height}')
+
+        self.right_frame = tk.Frame(self)
+        self.right_frame.grid(column=1, row=1, sticky='W')
+
+        self.left_frame = tk.Frame(self)
+        self.left_frame.grid(column=2, row=1, sticky='E')
+
+        self.lbl = tk.Label(self.right_frame, text="Data at t=0")
+        self.lbl.grid(column=1, row=1, pady=5, padx=10, sticky='NW')
+
+        self.lbl = tk.Label(self.right_frame, text="Velocity")
+        self.lbl.grid(column=0, row=3, pady=2, padx=10, sticky='NW')
+        self.vitesse = tk.Entry(self.right_frame, width=10)
+        self.vitesse.grid(column=1, row=3, pady=2, padx=10, sticky='NW')
+        self.comboExample = ttk.Combobox(self.right_frame,
+                                    values=[
+                                        "m/s",
+                                        "km/h",
+                                        "mi/s",
+                                        "mph"], width=5)
+        self.comboExample.grid(column=2, row=3, pady=2, sticky='NW')
+        self.comboExample.current(1)
+
+        self.lbl = tk.Label(self.right_frame, text="Angle")
+        self.lbl.grid(column=0, row=4, pady=2, padx=10, sticky='NW')
+        self.vitesse = tk.Entry(self.right_frame, width=10)
+        self.vitesse.grid(column=1, row=4, pady=2, padx=10, sticky='NW')
+        self.comboExample = ttk.Combobox(self.right_frame,
+                                        values=[
+                                            "teta",
+                                            "degree"], width=5)
+        self.comboExample.grid(column=2, row=4, pady=2, sticky='NW')
+        self.comboExample.current(1)
+
+        self.lbl = tk.Label(self.right_frame, text="Position")
+        self.lbl.grid(column=0, row=5, pady=2, padx=10, sticky='NW')
+        self.vitesse = tk.Entry(self.right_frame, width=10)
+        self.vitesse.grid(column=1, row=5, pady=2, padx=10, sticky='NW')
+
+        self.btn = tk.Button(self.right_frame, text=" View", width=20)
+        self.btn.grid(column=1, row=6, pady=10, sticky='NW')
+
+        self.cnv = tk.Canvas(self.left_frame, bg='white', bd=1, width=940, height=601, highlightbackground="grey")
+        self.cnv.grid(column=3, row=3, columnspan=3, rowspan=3, pady=20, padx=20, sticky='NE')
 
         # creating all the necessary widgets (labels, buttons, views, etc...)
 
-        #first step with git
+        # the matplotlib figure to put on the canva
+        """self.fig = Figure(figsize=(10, 10), dpi=100)
+        self.plot1 = self.fig.add_subplot(111)
+        self.plot1.plot(0, 0, "ko")
+
+        # the tkinter.canva element containing the matplotlib figure
+        self.canvas = FigureCanvasTkAgg(self.fig, self)
+        self.canvas.draw()
+
+        self.canvas.get_tk_widget().grid(column=3, row=8)
+        toolbar = NavigationToolbar2Tk(self.canvas, self)
+        toolbar.update()
+        self.canvas.get_tk_widget().grid(column=3, row=8)
+"""
 
 # testings 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     x, y = 601, 962
     my_window = Window(height=x, width=y)
     my_window.mainloop()
