551e8770f788a1345280a998ec6cf687c98be9e1
Ranto <ranto.rakotondrajoa@gmail.com>
2022-06-09 15:28:36 +0200

    restart work

--- interface/window.py
+++ interface/window.py
@@ -15,7 +15,7 @@ class Window(tk.Tk):
     @param height : default to 601, allows to create instance of the class with different height
     @param width : default to 962, allows to create instance of the class with different width
     """
-    def __init__(self, title="Window", height=601, width=962):
+    def __init__(self, title="Window", height=680, width=1306):
         # basic setup
         super().__init__()
         self.height = height
@@ -67,24 +67,7 @@ class Window(tk.Tk):
         self.cnv = tk.Canvas(self.left_frame, bg='white', bd=1, width=940, height=601, highlightbackground="grey")
         self.cnv.grid(column=3, row=3, columnspan=3, rowspan=3, pady=20, padx=20, sticky='NE')
 
-        # creating all the necessary widgets (labels, buttons, views, etc...)
-
-        # the matplotlib figure to put on the canva
-        """self.fig = Figure(figsize=(10, 10), dpi=100)
-        self.plot1 = self.fig.add_subplot(111)
-        self.plot1.plot(0, 0, "ko")
-
-        # the tkinter.canva element containing the matplotlib figure
-        self.canvas = FigureCanvasTkAgg(self.fig, self)
-        self.canvas.draw()
-
-        self.canvas.get_tk_widget().grid(column=3, row=8)
-        toolbar = NavigationToolbar2Tk(self.canvas, self)
-        toolbar.update()
-        self.canvas.get_tk_widget().grid(column=3, row=8)
-"""
 # testings 
 if __name__ == "__main__":
-    x, y = 601, 962
-    my_window = Window(height=x, width=y)
+    my_window = Window()
     my_window.mainloop()
